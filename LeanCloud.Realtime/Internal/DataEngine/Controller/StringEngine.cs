ceb0c46b3df5855d0ba9bf26ec1a9e2046fbf4e0
WuJun <wujun19890209@gmail.com>
2017-10-24 18:20:00 +0800

    chore: stage.

--- LeanCloud.Realtime/Internal/DataEngine/Controller/StringEngine.cs
+++ LeanCloud.Realtime/Internal/DataEngine/Controller/StringEngine.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace LeanCloud.Realtime.Internal
 {
     internal static class StringEngine
     {
-        internal static string Random(this string str,int length)
+        internal static string Random(this string str, int length)
         {
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
             var random = new Random();
             return new string(Enumerable.Repeat(chars, length)
               .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        internal static string TempConvId<T>(this IEnumerable<T> objs)
+        {
+            var base64Strs = 
         }
     }
 }
